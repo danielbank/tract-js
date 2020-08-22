b0551be3e89257b803136bd2d734d3f3b6ce15eb
Daniel Bank <daniel.p.bank@gmail.com>
2020-08-22 14:31:28 -0700

    Adding more tests

--- wrapper/tests/index.test.ts
+++ wrapper/tests/index.test.ts
@@ -1,31 +1,45 @@
 import { Model, Tensor, load, terminate } from "../dist/tractjs.js";
 
 describe('model', () => {
-  test('fails to load without input parameters', async () => {
+  afterAll((done) => {
+    terminate();
+    setTimeout(() => done(), 2000)
+  });
+
+  test('cannot be created directly', () => {
+    // @ts-ignore: Constructor of class 'Model' is private.
+    expect(() => new Model()).toThrow();
+  });
+
+  test('fails gracefully when loaded incorrectly', async () => {
     await expect(load('./tests/plus3.pb')).rejects.toThrow();
+    await expect(load('./tests/plus3', { optimize: false })).rejects.toThrow();
   });
 
-  test('fails to load with wrong input parameters', async (done) => {
-    await expect(load('./tests/plus3.pb', {
+  test('can load a model', async () => {
+    await expect(load('./tests/plus3.pb', { optimize: false })).resolves.toBeInstanceOf(Model)
+    await expect(load('./tests/plus3', { format: 'tensorflow', optimize: false })).resolves.toBeInstanceOf(Model)
+  });
+
+  test('can predict on a single input tensor', async () => {
+    const model: Model = await load('./tests/plus3.pb', {
       inputFacts: {
-        0: ['float32', [111, 2]],
+        0: ['float32', [1, 3]],
       },
-    })).rejects.toThrow();
+    });
+    const input = new Tensor(new Float32Array([1, 2, 3]), [1, 3]);
+    const prediction = await model.predict_one(input);
+    expect(Array.from(prediction.data)).toEqual([4, 5, 6]);
   });
 
-  test('loads with input parameters', async (done) => {
-    try {
-      const model: Model = await load('./tests/plus3.pb', {
-        inputFacts: {
-          0: ['float32', [1, 3]],
-        },
-      });
-      const tensor = new Tensor(new Float32Array([1.0, 2.0, 3.0]), [1, 3]);
-      const prediction = await model.predict([tensor]);
-      expect(prediction).toEqual([new Tensor(new Float32Array([4.0, 5.0, 6.0]), [1, 3])]);
-    } finally {
-      terminate();
-      done();
-    }
+  test('can predict on multiple input tensors', async () => {
+    const model: Model = await load('./tests/plus3.pb', {
+      inputFacts: {
+        0: ['float32', [1, 3]],
+      },
+    });
+    const input = new Tensor(new Float32Array([1, 2, 3]), [1, 3]);
+    const predictions = await model.predict([input]);
+    expect(Array.from(predictions[0].data)).toEqual([4, 5, 6]);
   });
 });

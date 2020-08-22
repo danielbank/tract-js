e99a0edb6c692b4395a37a889200897ead182f78
Daniel Bank <daniel.p.bank@gmail.com>
2020-08-22 10:12:07 -0700

    Adding a new test

--- wrapper/tests/index.test.ts
+++ wrapper/tests/index.test.ts
@@ -1,12 +1,38 @@
-import { Model, terminate } from "../dist/tractjs.js";
+import { Model, Tensor, terminate } from "../dist/tractjs.js";
 
 describe('model', () => {
-  test('fails to load without input parameters', async (done) => {
+  test('fails to load without input parameters', async () => {
     const model: Model = new Model('./tests/plus3.pb');
-    await expect(model.modelId).rejects.toThrow();
+    await expect(model.loaded()).rejects.toThrow();
     terminate();
-    setTimeout(() => {
+  });
+
+  jest.setTimeout(100000);
+  test('loads with input parameters', async (done) => {
+    /*
+          inputFacts: {
+        0: ['float32', [1, 224, 224, 3]],
+      },
+    */
+    const model: Model = new Model('./tests/plus3.pb', {
+      optimize: false,
+    });
+    try {
+      console.log('foo');
+      await model.loaded();
+      console.log('bar');
+      const tensor = new Tensor(new Float32Array([1.0, 2.0, 3.0]), [1, 3]);
+      console.log('predicting');
+      const prediction = await model.predict([tensor]);
+      console.log('prediction: ', prediction);
+      // console.log('prediction: ', prediction[0]);
+      // expect(prediction).toEqual(4);
+      // done();
+    } catch (err) {
+      console.log(err);
+    } finally {
+      terminate();
       done();
-    }, 2000)
+    }
   });
 });

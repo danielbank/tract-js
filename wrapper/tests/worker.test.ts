9016fc746c988cbaf36a2cff8ff5c8950c49a7c6
Daniel Bank <daniel.p.bank@gmail.com>
2020-08-22 16:30:45 -0700

    Worker test is a black box

--- wrapper/tests/worker.test.ts
+++ wrapper/tests/worker.test.ts
@@ -1,24 +1,31 @@
 import Worker from "web-worker";
 import datauri from 'datauri';
+import fs from "fs";
 
 describe('worker', () => {
   let worker: Worker;
 
   beforeAll(async () => {
-    worker = new Worker(await datauri("./dist/worker.js") as string);
+    worker = new Worker(await datauri("./dist/worker.js"));
   });
 
-  test('loads the worker with errors', (done) => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const handler = (e: any) => {
+  test('can load a model', async (done) => {
+    const modelData = await fs.promises.readFile("./tests/plus3.pb");
+    const options = {
+      optimize: false,
+      format: 'tensorflow',
+      inputFacts: {},
+    };
+
+    const handler = (e) => {
       expect(e.data.uid).toEqual(1);
-      expect(e.data.type).toEqual("error");
-      expect(e.data.body.message).toEqual("invalid_argument");
+      expect(e.data.type).toEqual("load");
+      expect(e.data.body).toEqual(0);
       worker.removeEventListener("message", handler);
       done()
     };
     worker.addEventListener("message", handler);
-    worker.postMessage({ type: 'load', body: { data: 1, options: {} }, uid: 1 });
+    worker.postMessage({ type: 'load', body: { data: modelData, options }, uid: 1 });
   });
 
   afterAll(() => {
